06c8a5dfd99a5225c7bd9bb0a1c25eac271f4895
Raenah <70087854+Raenah@users.noreply.github.com>
2020-11-11 00:32:31 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,58 @@
+var dog,dogimage; 
+var happyDog,happyDogimage; 
+var database;
+var foodS, foodStock;
+
+function preload()
+{
+  dogimage = loadImage("dogImg.png");
+  happyDogimage = loadImage("dogImg1.png");
+}
+
+function setup() {
+  createCanvas(500,500);
+  
+ dog = createSprite(50,180,20,50);
+ dog.addImage("dog",dogimage);
+ 
+
+ database = firebase.database();
+
+ foodStock=database.ref('Food');
+ foodStock.on("value",readStock);
+}
+
+function draw() {  
+  background(46,139,87);
+
+  
+ if(keyWentDown(UP_ARROW)){
+   writeStock(foodS);
+   dog.addImage("happyDog",happyDogimage)
+   }
+
+drawSprites();
+ text();
+ textSize(12);
+ fill();
+ stroke();
+
+}
+
+function readStock (data)  {
+ foodS = data.val();
+}
+
+function writeStock (x)  {
+  if (x<=0) {
+    x=0
+  } else {
+    x=x-1;
+  }
+
+  database.ref('/').update({
+    Food:x
+  })
+}
+
+
